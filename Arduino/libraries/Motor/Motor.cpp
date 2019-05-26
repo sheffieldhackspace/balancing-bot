d2a4d8d8ace53a8890afb4b89a21d632bf2eca92
Philip <49306802+tennessee1227@users.noreply.github.com>
2019-05-26 14:37:41 +0100

    Update Motor.cpp

--- Arduino/libraries/Motor/Motor.cpp
+++ Arduino/libraries/Motor/Motor.cpp
@@ -20,34 +20,43 @@ Motor::Motor(int ain1, int ain2, int stby, int pwmpin, float ndb, float pdb, flo
 	pinMode(_pwmpin, _pwmpin);
  	digitalWrite(_stby, HIGH);
 }
-void Motor::speed(float speed){
+void Motor::speed(float speed)
+	{
 	_speed = speed;
-	if (_speed < 0){
+	if (_speed < 0)
+		{
 		digitalWrite(_ain1, HIGH);
 		digitalWrite(_ain2, LOW);
 		_intspeed = _speed*_mps_scalefactor-_ndb;
         
-        if (_intspeed < -255){
+        	if (_intspeed < -255){
 			_intspeed = -255;
-}
+		}
 
 		analogWrite(_pwmpin, -1*(_intspeed));
-        Serial.println(_intspeed);
+		
+#ifndef USE_HARDWARE_SERIAL 
+    		Serial.println(_intspeed);
+#endif    
 
 	}
-	else {
-
+	else 
+	{
 		digitalWrite(_ain1, LOW);
 		digitalWrite(_ain2, HIGH);
-        _intspeed = _speed*_mps_scalefactor+_pdb;
+        	_intspeed = _speed*_mps_scalefactor+_pdb;
                 
-	if (_intspeed > 255){
-		_intspeed = 255;
-}
+		if (_intspeed > 255)
+		{
+			_intspeed = 255;
+		}
 		analogWrite(_pwmpin, _intspeed);
-        Serial.println(_intspeed);
+		
+#ifndef USE_HARDWARE_SERIAL 
+    		Serial.println(_intspeed);
+#endif  
         
-    }
+    	}
 	
 
 }
